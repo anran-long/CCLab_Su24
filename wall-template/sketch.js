c13cf14a58d06861d27551fb91dec491f7955d00
anran-long <al7902@nyu.edu>
2024-07-22 09:50:32 -0700

    Update sketch.js

--- wall-template/sketch.js
+++ wall-template/sketch.js
@@ -1,4 +1,10 @@
+let resetButton;
 let x = 100
+let jiggling = false;
+let offsetX = 0;
+let jigglingAmount = 500;
+
+
 
 let numChunks = 25;
 let chunkX = [];
@@ -17,8 +23,8 @@ let showChunksCountdown = false;
 let frameCountAtLastChunkClick = 0;
 
 function setup() {
-    let cnv=createCanvas(800, 500);
-    cnv.parent("p5-canvas-container")
+  let cnv=createCanvas(800, 500);
+  cnv.parent("p5-canvas-container")
   
   // Random chunks
   for (let i = 0; i < numChunks; i++) {
@@ -29,6 +35,10 @@ function setup() {
     chunkSpeed[i] = random(0.1, 0.3);
     sinInput[i] = random(0, 360);
   }
+  resetButton = createButton('REENTER');
+  resetButton.position(360, 185);
+ resetButton.mousePressed(resetSketch);
+ resetSketch();
 }
 
 function draw() {
@@ -38,7 +48,7 @@ function draw() {
 
   // wall line
   stroke(80, 50, 50, 50);
-  strokeWeight(8);
+  strokeWeight(15);
   line(width * 0.25, height - 150, width * 0.25, 0);
   line(width * 0.75, height - 150, width * 0.75, 0);
 
@@ -68,15 +78,28 @@ function draw() {
     let sinValue = sin(sinInput[i]);
     sinInput[i] += 0.08;
     let shake = sinValue * amp;
+    let d = dist(mouseX, mouseY, chunkX[i], chunkY[i]);
 
-    rect(chunkX[i] + shake, chunkY[i], chunkW[i], chunkH[i]);
+    rect(chunkX[i] + shake + offsetX, chunkY[i], chunkW[i], chunkH[i]);
 
     // Chunks fall until they reach the ground
     chunkY[i] += chunkSpeed[i];
     if (chunkY[i] + chunkH[i] >= height) {
       chunkY[i] = height - chunkH[i];
     }
+      if (d < 100) { // If mouse is within 100 pixels
+    jiggling = true;
+  } else {
+    jiggling = false;
+    offsetX = 0;
   }
+  if (jiggling==true) {
+    // Apply a jiggling effect
+    offsetX = sinValue*jigglingAmount 
+  }
+  }
+
+
   // wall texture
   if (
     mouseY < height - 150 &&
@@ -87,7 +110,7 @@ function draw() {
     for (let y = 0; y < height-150; y += 50) {
       for (let x = 0; x < width; x += 50) {
         noStroke();
-        fill("#FAF3DF1E");
+        fill("#FAF3DF0F");
         rect(x, y, random(-50, 50), random(-50, 50));
       }
     }
@@ -95,10 +118,8 @@ function draw() {
   
 //click on chunks on the ground--->message  
 
-
-
   let framesSinceLastClick = frameCount-frameCountAtLastChunkClick
-  if (framesSinceLastClick>30 && showChunkText == true && showcountdown == true){
+  if (framesSinceLastClick>35 && showChunkText == true && showcountdown == true){
     showChunkText = false;
     showcountdown = true;
     // delete first element in texts array
@@ -106,15 +127,14 @@ function draw() {
     countdown.splice(0,1)
   }
   
-  
   if(showChunkText == true){
-    fill ("rgb(138,22,22)")
+    fill ("#311C05")
     textSize (20)
-    text(texts[0],205,190)
+    text(texts[0],205,175)
   }
   
   if (showcountdown == true){
-    fill ("rgb(138,22,22)")
+    fill ("#311C05")
     textSize (40)
     text(countdown[0],100,175)
   }
@@ -130,42 +150,37 @@ function draw() {
   fill("#F3EEE1")
   rect(width*0.25,173,width*0.5,20)
   textSize(20)
-  fill("rgb(138,22,22)")
+  fill(0)
   text("TIME TO REFURNISH THE HOUSE",235,190)
  }
 
   drawSofa(x,50)
-  if (mouseX<110+x && mouseX>80+x && mouseY<300 && mouseY>200){
+  if (mouseX<150+x && mouseX>40+x && mouseY<300 && mouseY>160){
   x+=1.5
   }
-  if (mouseX<280+x && mouseX>220+x && mouseY<300 && mouseY>200){
+  if (mouseX<320+x && mouseX>180+x && mouseY<300 && mouseY>160){
   x-=1.5
   }
-textSize(15) 
-fill("#FAF3DF9E")
-text("move sofa",x+145,365)
+  
 
 }
 function drawSofa(x) {
   push()
   translate(x,50)
-  stroke(0)
+  stroke("#DACDAA42")
   strokeWeight(2)
-  // Sofa base
-  fill(0,90); 
-  rect(100, 250, 160, 50, 20); // Base of the sofa
   // Sofa cushions
-  fill(0,90); 
-  rect(110, 200, 140, 60, 10); // Back cushion
-  rect(120, 250, 60, 50, 10);  // Left cushion
+  fill("#DACDAA"); 
+  rect(111, 200, 140, 60, 10); // Back cushion
+  // Sofa base
+  fill("#483027"); 
+  rect(100, 250, 160, 50, 20); // Base of the sofa
+  fill("#C8491A7A");
+  rect(120, 230, 70, 60, 10);  // Left cushion
   // Armrests
-  fill(0,97);
+  fill("#311C05");
   rect(90, 250, 20, 50, 10);   // Left armrest
-  rect(260, 250, 20, 50, 10);  // Right armrest
-  // Legs
-  fill("rgb(0,0,0)"); 
-  rect(100, 300, 20, 10);  // Left leg
-  rect(260, 300, 20, 10);  // Right leg  
+  rect(255, 250, 20, 50, 10);  // Right armrest
 pop()
 
 }
@@ -188,7 +203,7 @@ function mousePressed() {
       frameCountAtLastChunkClick = frameCount;
       
       // delete chunk, donot show text
-    if (mouseY<height-100){
+    if (mouseY<height-200){
       chunkX.splice(i,1)
       chunkY.splice(i,1)
       chunkW.splice(i,1)
@@ -208,5 +223,34 @@ function mousePressed() {
       showChunksCountdown=true
     }
   }
+  }
+}
+
+//RESTART
+function resetSketch() {
+  x = 100;
+  chunkX = [];
+  chunkY = [];
+  chunkW = [];
+  chunkH = [];
+  chunkSpeed = [];
+  sinInput = [];
+  showChunkText = false;
+  showcountdown = false;
+  showChunksCountdown = false;
+  frameCountAtLastChunkClick = 0;
+  
+  texts = ["你好呀 does it feel scary?", "this is getting weird","life is like a haunted house haha","guess who am i?","turning into those little chunkies","i hate the molds growing in here","it's time to refurnish this old house","cherish every moment of your life","最后,记得","我爱你"];
+  countdown = ["10","9","8","7","6","5","4","3","2","1"];
+  ChunksCountdown = ["25","24","23","22","21","20","19","18","17","16","15","14","13","12","11","10","9","8","7","6","5","4","3","2","1"];
+  
+  // Random chunks
+  for (let i = 0; i < numChunks; i++) {
+    chunkX[i] = random(width);
+    chunkY[i] = random(height);
+    chunkW[i] = random(20, 50);
+    chunkH[i] = random(20, 50);
+    chunkSpeed[i] = random(0.1, 0.3);
+    sinInput[i] = random(0, 360);
   }
 }
